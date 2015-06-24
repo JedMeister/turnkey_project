a5afc20f7bb60072a46932b634b8398bb8127e6a
Jeremy Davis <jeremy@turnkeylinux.org>
2015-06-24 05:48:19 +0000

    updated to serve simple turnkey app

--- turnkey_project/settings.py
+++ turnkey_project/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'turnkey',
 )
 
 MIDDLEWARE_CLASSES = (
--- turnkey_project/urls.py
+++ turnkey_project/urls.py
@@ -1,11 +1,12 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from turnkey import views
 
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'turnkey_project.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-    url(r'^$', 'views.index', {"template": "index.html"}, name="index"),
+    url(r'^$', views.index, name="index"),
 
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     url(r'^admin/', include(admin.site.urls)),
