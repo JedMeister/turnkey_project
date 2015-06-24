e0f686630d57be39c948a2ed2b7903b870fb5c43
Jeremy Davis <jeremy@turnkeylinux.org>
2015-06-24 04:03:53 +0000

    added old config to new branch

--- turnkey_project/settings.py
+++ turnkey_project/settings.py
@@ -1,83 +1,80 @@
-"""
-Django settings for turnkey_project project.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/1.7/topics/settings/
-
-For the full list of settings and their values, see
-https://docs.djangoproject.com/en/1.7/ref/settings/
-"""
-
-# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
-BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+import sys
 
+DEBUG = False
+TEMPLATE_DEBUG = False
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/1.7/howto/deployment/checklist/
+ADMINS = (
+    ('Django Admin', 'admin@example.com'),
+)
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'v#)8+^$8%s^^w4t$a)=j(aemi2(uyy#-=yl8)*fsudg$w-6e@)'
+MANAGERS = ADMINS
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
+sys.path.insert(0, os.path.join(PROJECT_ROOT, "apps"))
 
-TEMPLATE_DEBUG = True
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'django',
+        'USER': 'django',
+        'PASSWORD': '6fae8f77be618308dab249872fa71738',
+    }
+}
 
-ALLOWED_HOSTS = []
+EMAIL_HOST = "localhost"
+EMAIL_PORT = "25"
+DEFAULT_FROM_EMAIL = "noreply@example.com"
 
+TIME_ZONE = 'UTC'
+LANGUAGE_CODE = 'en-us'
+SITE_ID = 1
+USE_I18N = False
 
-# Application definition
+DOC_ROOT = '/usr/share/doc/python-django-doc/html'
+
+MEDIA_ROOT = os.path.join(PROJECT_ROOT, 'media')
+MEDIA_URL = '/media/'
+STATIC_URL = '/media/admin/'
+
+SECRET_KEY = 'h__b!q!i6npx%@e@((w5+$_dq9d&v+maogm87edx_@978$=!=g'
+
+TEMPLATE_LOADERS = (
+    ('django.template.loaders.cached.Loader', (
+        'django.template.loaders.filesystem.Loader',
+        'django.template.loaders.app_directories.Loader',
+    )),
+)
+
+MIDDLEWARE_CLASSES = (
+    'django.middleware.common.CommonMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+)
+
+ROOT_URLCONF = 'urls'
+
+TEMPLATE_CONTEXT_PROCESSORS = (
+    'django.core.context_processors.request',
+    'django.contrib.auth.context_processors.auth',
+    'django.contrib.messages.context_processors.messages',
+)
+
+TEMPLATE_DIRS = (
+    os.path.join(PROJECT_ROOT, "templates"),
+)
 
 INSTALLED_APPS = (
-    'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
+    'django.contrib.sites',
+    'django.contrib.admin',
+    'django.contrib.admindocs',
 )
 
-MIDDLEWARE_CLASSES = (
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
-)
+if os.environ.get('DEVELOPMENT', None):
+    from settings_dev import *
 
-ROOT_URLCONF = 'turnkey_project.urls'
-
-WSGI_APPLICATION = 'turnkey_project.wsgi.application'
-
-
-# Database
-# https://docs.djangoproject.com/en/1.7/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-
-# Internationalization
-# https://docs.djangoproject.com/en/1.7/topics/i18n/
-
-LANGUAGE_CODE = 'en-us'
-
-TIME_ZONE = 'UTC'
-
-USE_I18N = True
-
-USE_L10N = True
-
-USE_TZ = True
-
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.7/howto/static-files/
-
-STATIC_URL = '/static/'
