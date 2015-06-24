3f22524239dc6fb15a3bc81f072b7f9a83e7f87f
Jeremy Davis <jeremy@turnkeylinux.org>
2015-06-24 05:20:27 +0000

    updated urls...

--- turnkey_project/urls.py
+++ turnkey_project/urls.py
@@ -5,6 +5,9 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'turnkey_project.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
+    url(r'^$', 'views.index', {"template": "index.html"}, name="index"),
 
+    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     url(r'^admin/', include(admin.site.urls)),
+    
 )
